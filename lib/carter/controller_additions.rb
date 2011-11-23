c9075e07ca5c202c815fa4cf47263bd44cc0b9f9
Louis Gillies <louisgillies@yahoo.co.uk>
2011-11-23 12:59:11 +0000

    refactor to invoke load_cart filter by symbol so it can be skipped.

--- lib/carter/controller_additions.rb
+++ lib/carter/controller_additions.rb
@@ -6,12 +6,13 @@ module Carter
     module ClassMethods
       
       def has_cart(*args)
-        ControllerResource.add_before_filter(self, :load_cart, *args)
+        has_cart_options = args.extract_options!
+        before_filter :load_cart, has_cart_options.slice(:only, :except)
         include InstanceMethods
       end
 
       def has_cart_for_checkout(*args)
-        ControllerResource.add_before_filter(self, :load_cart_for_checkout, *args)
+        before_filter :load_cart_for_checkout, args.extract_options!.slice(:only, :except)
         include InstanceMethods
       end
 
@@ -21,6 +22,7 @@ module Carter
       base.extend ClassMethods
       base.class_attribute :checking_out
       base.class_attribute :shopping
+      base.class_attribute :has_cart_options
       base.helper_method :cart, :has_cart?, :shopper
     end
     
@@ -43,7 +45,7 @@ module Carter
       end
       
       def continue_shopping_or_default_url(default)
-        session[:continue_shopping_url] || default || root_url
+        session[:continue_shopping_url] || default
       end
       
       # to stop the has_cart method called from super class from setting the continue_shopping_url when checking out.
@@ -53,6 +55,49 @@ module Carter
       
       def shopping?
         shopping == true
+      end
+      
+
+      def load_cart
+        store_shopping_location if self.shopping? && !self.checking_out?
+        @cart = load_cart_instance
+        @shopper = current_shopper
+      end
+
+      def load_cart_for_checkout
+        @cart = load_cart_instance
+        @shopper = current_shopper
+      end
+
+      private
+
+      def load_cart_instance
+        find_cart_by_session || find_cart_by_shopper  || ::Cart.new
+      end
+
+
+      def find_cart_by_shopper
+        ::Cart.with_state(default_cart_state).first(:conditions => {:shopper_id => current_shopper.id, :shopper_type => current_shopper.class.name} ) if current_shopper && Carter.settings.persist_by_shopper?
+      end
+
+      def find_cart_by_session
+        ::Cart.with_state(default_cart_state).find_by_id(session[:cart_id]) 
+      end
+
+      def current_shopper
+        self.respond_to?(:current_user) ? self.send(:current_user) : nil
+      end
+
+      def store_shopping_location
+        session[:continue_shopping_url] = self.request.fullpath 
+      end
+
+      def name_from_carter_controller
+        params[:controller].sub("Controller", "").underscore.split('/').last.singularize
+      end
+
+      def default_cart_state
+        :active
       end
       
       protected
